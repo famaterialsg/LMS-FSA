c492a75c94eeae12d33199f1dac29c530bcca602
NVkhang26 <nguyenvinhkhang26042003@gmail.com>
2024-12-07 15:54:27 +0700

    update main ngay 12/7 nhe cô

--- main/templatetags/main_form_filters.py
+++ main/templatetags/main_form_filters.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter(name='add_class')
 def add_class(value, css_class):
     return value.as_widget(attrs={'class': css_class})
+@register.filter
+def remove_list(value):
+    if isinstance(value, str):
+        return value.replace('list', '').strip()
+    return value
